a0d27caaa628c376a773eca99c1b505e50aa44c3
Cry0 <cryogen65@gmail.com>
2022-11-18 22:53:00 -0500

    added ai query

--- app/backend/run.py
+++ app/backend/run.py
@@ -1,7 +1,37 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for
+import openai
 
+
+openai.api_key = "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"
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return "Hello World"
+
+@app.route('/generate_response', methods=['GET'])
+def generate_response():
+    # openai request
+    response = openai.Completion.create(
+      model="text-curie-001",
+      prompt="Create a list of 8 questions for my interview with a C++ software engineer and format it as a Python list:",
+      temperature=0.9,
+      max_tokens=150,
+      top_p=1,
+      frequency_penalty=0.0,
+      presence_penalty=0.6,
+      stop=[" Human:", " AI:"]
+    )
+    response_str = response['choices'][0]['text']
+    # remove  1., 2., 3., 4., 5., 6., 7., 8. from response
+    response_str = response_str.replace('1.', '').replace('2.', '').replace('3.', '').replace('4.', '').replace('5.', '').replace('6.', '').replace('7.', '').replace('8.', '')
+    response_ls = response_str.splitlines()
+    response_ls = [x.strip() for x in response_ls]
+    print(f"response_ls: {response_ls}")
+    # replace all single quotes with double quotes
+    response_ls = [x.replace("'", '"') for x in response_ls]
+    print(f"response_ls: {response_ls[3]}")
+    for response in response_ls:
+        print(f"response: {response}")
+    # return render_template('index.html', response=response_ls)
+    return response_ls
