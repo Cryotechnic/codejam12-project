4ae93e748da81c16282216da8bfce12dec107d29
Raagav Prasanna <raagav.pras@gmail.com>
2022-11-19 03:42:38 -0500

    Fixed missing information displayed for job offer modal

--- app/src/JobModel.jsx
+++ app/src/JobModel.jsx
@@ -6,9 +6,16 @@ export default function JobModel(props) {
     <div className = "popup-box">
       <div className = "box">
       <span className="close-icon" onClick={props.handleClose}>x</span>
-        <p>{props.data.job_id}</p>
-        <p>{props.data.job_title}</p>
-        <p>{props.data.job_company}</p>
+        <p><b>Job ID: </b>{props.data.job_id}</p>
+        <p><b>Job Title: </b>{props.data.job_title}</p>
+        <p><b>Company: </b>{props.data.job_company}</p>
+        <p><b>Job Description: </b>{props.data.job_description}</p>
+        <p><b>Job Skills: </b></p>
+        <ul>
+          {props.data.job_skills.map((obj, index) => {
+            return <li key={index}> {obj} </li>
+          })}
+        </ul>
       </div>
     </div>
   )
