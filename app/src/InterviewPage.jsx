041df8629574a337a0cdabcb1044b0ae898bfb8f
iyers16 <shanmukh.iyer@gmail.com>
2022-11-19 08:05:08 -0500

    Recieved API info and displaying it in chatbot

--- app/src/InterviewPage.jsx
+++ app/src/InterviewPage.jsx
@@ -1,34 +1,52 @@
 import React, {useState} from 'react';
-// import ChatBot from 'react-simple-chatbot';
+import ChatBot from 'react-simple-chatbot'
 import { useLocation } from 'react-router-dom';
+import { script } from "./chatbot";
+import { stepifyScript } from "./utils";
 
-const generateRes = 'http://localhost:5000/generate_response';
-async function getDataFromBackend() {
-    // require cors
-    const response = await fetch(generateRes, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    });
-    const data = await response.json();
-    return data;
-}
+// const originalData = [
+//     "What experience do you have with C++?",
+//     "What are some of the challenges you have faced with C++ programming?",
+//     "What is your experience with object-oriented programming in C++?",
+//     "What do you know about using templates in C++?",
+//     "What libraries or frameworks have you used in your C++ projects?",
+//     "What is your experience with algorithms and data structures in C++?",
+//     "What are some of the design patterns that you are familiar with?",
+//     "What do you think makes C++ a powerful tool for software engineering?"
+// ]
+  
+
+// const generateRes = 'http://localhost:5000/generate_response';
+// async function getDataFromBackend() {
+//     // require cors
+//     const response = await fetch(generateRes, {
+//         method: 'GET',
+//         headers: {
+//             'Content-Type': 'application/json',
+//         },
+//     });
+//     return await response.json();
+// }
 
 export default function InterviewPage() {
     const location = useLocation();
     const id = location.state.id;
     const job = location.state.job;
     const company = location.state.company;
+    const data = location.state.data;
+    
+    function handleEnd({ steps, values }) {
+        console.log(values);
+    }
 
-    const [data, setData] = useState(null);
+    const conSteps = [];
+    let ctr = 0;
 
     return (
         <div>
 
             <h1>Interview Page {id} for {job} at {company}</h1>
-            <button onClick={() => {
-                
+            {/* <button onClick={() => {
                 getDataFromBackend()
                 .then((data) => {
                     data.map((item) => {
@@ -36,29 +54,13 @@ export default function InterviewPage() {
                     });
                     setData(data);
                 });
-            }} >Trigger</button>
-            <p>{data}</p>
+            }} >Trigger</button> */}
+            {<p>{data}</p>}
+            <ChatBot
+                handleEnd={handleEnd}
+                steps={conSteps}
+            />
 
-            {/* <ChatBot
-                steps={[
-                    {
-                        id: '1',
-                        message: 'What is your name?',
-                        trigger: '2',
-                    },
-                    {
-                        id: '2',
-                        user: true,
-                        trigger: '3',
-                    },
-                    {
-                        id: '3',
-                        message: 'Hi {previousValue}, nice to meet you!',
-                        end: true,
-                    },
-                ]}
-                style={{ width: '90%', padding: '10px', margin: 'auto' }}
-            /> */}
         </div>
     );
 }
--- app/src/Job.jsx
+++ app/src/Job.jsx
@@ -1,8 +1,26 @@
-import React from 'react';
-import {Route, Routes, Link} from 'react-router-dom';
-import InterviewPage from './InterviewPage';
+import React, {useState} from 'react';
+import {Link} from 'react-router-dom';
+
+const generateRes = 'http://localhost:5000/generate_response';
+async function getDataFromBackend() {
+    // require cors
+    const response = await fetch(generateRes, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+    return await response.json();
+}
 
 export default function Job(props) {
+    const [data, setData] = useState(null);
+    const sendToInterview = () => {
+        getDataFromBackend()
+        .then((data) => {
+            setData(data);
+        });
+    }
   return (
       <div>
         <p>{props.data.id}</p>
@@ -12,11 +30,9 @@ export default function Job(props) {
         <Link to="/interview" state={{
             id: props.data.id,
             job: props.data.job,
-            company: props.data.company
-        }} ><button>Apply</button></Link>
-        {/* <Routes>
-                <Route path="/interview" element={<InterviewPage/>}/>
-        </Routes> */}
+            company: props.data.company,
+            data: data
+        }} ><button onClick={() => sendToInterview()}  >Apply</button></Link>
         
       </div>
   )
