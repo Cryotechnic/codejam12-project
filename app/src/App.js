045fa8d93366517d347bc83d8bfcfaf124ad61c9
Raagav Prasanna <raagav.pras@gmail.com>
2022-11-18 22:00:33 -0500

    Did jack shit

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,14 @@
 import './styles/App.css';
 
+
+const jobData = [{title: "asdgjkasdf", id:7, company: "hdsghdsfh"}, {title: "fjjdhfjdsadnsadghsdf", id:8, company: "fodsjgghdsfh"}, {title: "dhfssjdfhdljkkjsgdh", id:9, company: "hfjdfhjghdsfh"}, {title: "hdsgusdghljkkjsgdh", id:10, company: "hfdjhhdsfh"}]
+
 function App() {
   return (
     <div className="App">
-        <h1>Landing page</h1>
+      <table>
+          <th>Jobs</th>
+      </table>
     </div>
   );
 }
