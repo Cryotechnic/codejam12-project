6c66094ed1939501fca98e314be58cd3620ef48c
Raagav Prasanna <raagav.pras@gmail.com>
2022-11-19 00:18:22 -0500

    Jobs display

--- app/src/Landing.jsx
+++ app/src/Landing.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import Job from './Job'
+
+let items = [{job: "Programmer", id:1, company: "Google"}, {job: "Gamer", id:2, company: "Ubisoft"}, {job: "Webber", id:3, company: "Marvel"}];
 
 export default function Landing() {
   return (
-    <p> Panchod </p>
+    <div>
+        {items.map((obj, index) => {
+          return <div key={index}> <Job data={obj}/> </div>
+        })}
+    </div>
   );
 }
