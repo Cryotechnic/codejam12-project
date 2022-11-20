6cdfd45b82a79ce1dc5d36c97b26ce0ddf4ff0c9
Cry0 <cryogen65@gmail.com>
2022-11-20 00:51:09 -0500

    applied offset for site title (for real this time)

--- app/src/Landing.jsx
+++ app/src/Landing.jsx
@@ -90,7 +90,7 @@ export default function Landing() {
 
     return (
       <div>
-        <h1 style={{textAlign: "center"}}>JobSmart</h1>
+        <h1 style={{textAlign: "center", paddingTop: '1.0vh', paddingBottom: '1.0vh'}}>JobSmart</h1>
         <TableContainer sx={{mx:'auto', width: '52%'}} component={Paper} style={{borderRadius: 12.5}}>
           <Table sx={{minWidth: 650, width: '100%'}} size="small" aria-label="simple table">
             <TableHead>
